706f9a9df5684b12547cac795156a46efdeff484
maxreis86 <maxreis86@gmail.com>
2022-09-11 22:15:34 -0300

    v3

--- sagemaker-custom-image/deploy_docker.sh
+++ sagemaker-custom-image/deploy_docker.sh
@@ -1,5 +1,4 @@
-dockerd
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 245799943033.dkr.ecr.us-east-1.amazonaws.com
-docker build -t teste2 .
-docker tag teste2:latest 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste2:latest
-docker push 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste2:latest
+docker build -t teste3 .
+docker tag teste3:latest 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste3:latest
+docker push 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste3:latest
