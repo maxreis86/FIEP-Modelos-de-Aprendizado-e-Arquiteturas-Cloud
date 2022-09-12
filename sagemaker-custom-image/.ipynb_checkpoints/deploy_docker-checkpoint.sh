8abfc80bf3d9a6b5f06bc34f975d6ee25c421288
maxreis86 <maxreis86@gmail.com>
2022-09-11 22:12:33 -0300

    v2

--- sagemaker-custom-image/.ipynb_checkpoints/deploy_docker-checkpoint.sh
+++ sagemaker-custom-image/.ipynb_checkpoints/deploy_docker-checkpoint.sh
@@ -1,5 +1,5 @@
-sudo dockerd
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 245799943033.dkr.ecr.us-east-1.amazonaws.com
-sudo docker build -t teste2 .
-sudo docker tag teste2:latest 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste2:latest
-sudo docker push 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste2:latest
+dockerd
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 245799943033.dkr.ecr.us-east-1.amazonaws.com
+docker build -t teste2 .
+docker tag teste2:latest 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste2:latest
+docker push 245799943033.dkr.ecr.us-east-1.amazonaws.com/teste2:latest
