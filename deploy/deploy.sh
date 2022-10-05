746e83a4681a6fc1eab3abddfe4aa3c3f5ac7b2d
maxreis86 <maxreis86@gmail.com>
2022-10-04 21:34:21 -0300

    deploy Intera

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
-# serverless config credentials --provider aws --key AKIATSOWRPN4647AE5NC --secret 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
-serverless create --template aws-python3 --name random-recommendation
+#serverless config credentials --provider aws --key AKIATSOWRPN4647AE5NC --secret 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
+serverless create --template aws-python3 --name model-prop-apply-prospects
 serverless plugin install -n serverless-python-requirements
 python handler.py
 #serverless invoke local --function predict-default --path event.json
