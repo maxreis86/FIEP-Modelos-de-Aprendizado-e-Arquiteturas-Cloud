fd9ec0809fea7eeccf8113b09145b3a4521eefe5
Maxuel Reis <maxreis86@gmail.com>
2022-10-14 00:58:25 +0000

    deploy do melhor modelo

--- titanic_web_app/js/app.js
+++ titanic_web_app/js/app.js
@@ -1,5 +1,4 @@
 const predictEndpoint = 'https://5a51rpxvrj.execute-api.us-east-1.amazonaws.com/prod/get-predict?';
-// const predictEndpoint = 'https://5a51rpxvrj.execute-api.us-east-1.amazonaws.com/prod/get-predict?';
 
 $('#ProbLoader').hide();
 $('#RatingLoader').hide();
